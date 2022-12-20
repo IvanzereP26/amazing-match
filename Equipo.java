07e8b6a38d2031d53a84685c10abd01664690963
IvazereP26 <ivanmartinperez2601@gmail.com>
2022-12-20 12:27:36 +0100

    Primer commit

---
+++ Equipo.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Equipo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Equipo
+{
+    // instance variables - replace the example below with your own
+    private String nombreEquipo;
+    private int anioFundacion;
+
+    /**
+     * Constructor for objects of class Equipo
+     */
+    public Equipo(String nombreDeEquipo, int anioDeLaFundacion){
+        nombreEquipo = nombreDeEquipo;
+        anioFundacion = anioDeLaFundacion;
+    }
+}
