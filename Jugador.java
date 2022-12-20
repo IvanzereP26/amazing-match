1e64b54e99c17a346d3b37db926dd06350b299c7
IvazereP26 <ivanmartinperez2601@gmail.com>
2022-12-20 13:00:36 +0100

    Quinto commit

--- Jugador.java
+++ Jugador.java
@@ -3,4 +3,9 @@ public class Jugador
     private String nombreJugador;
     private int dorsal;
     private Equipo estadio;
+    public Jugador (String nombreDelJugador, int dorsalJugador, Equipo nombreEstadio){
+        nombreJugador = nombreDelJugador;
+        dorsal = dorsalJugador;
+        estadio = nombreEstadio;
+    }
 }
